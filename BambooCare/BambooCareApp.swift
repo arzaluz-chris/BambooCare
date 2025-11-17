7402a7068abe5d06de0a589149df47f19b55f0ff
arzaluz-chris <23008381@es.uveg.edu.mx>
2025-11-17 16:52:48 -0600

    Initial Commit

---
+++ BambooCare/BambooCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  BambooCareApp.swift
+//  BambooCare
+//
+//  Created by Christian Arzaluz on 17/11/25.
+//
+
+import SwiftUI
+
+@main
+struct BambooCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
